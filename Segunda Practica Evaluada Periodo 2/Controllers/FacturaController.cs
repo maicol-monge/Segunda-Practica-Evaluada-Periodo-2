dd6b243933c7664d2239009a553f11ef6614179f
Ever Zamora <ever.zamora@catolica.edu.sv>
2024-10-05 22:31:03 -0600

    hhhhhh

--- Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
+++ Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
@@ -7,6 +7,14 @@ namespace Segunda_Practica_Evaluada_Periodo_2.Controllers
 		public IActionResult Index()
 		{
 			return View();
+			int a = 2;
+			if (a > 1)
+			{
+
+			}
+			//Hola
+			//fegfgesg
+
 		}
 	}
 }
