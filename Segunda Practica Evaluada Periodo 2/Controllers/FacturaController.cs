c574de317efe8861b5c5f34984fe666951ea2754
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-05 23:14:57 -0600

    Casi Casi

--- Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
+++ Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Segunda_Practica_Evaluada_Periodo_2.Models;
 
 namespace Segunda_Practica_Evaluada_Periodo_2.Controllers
 {
 	public class FacturaController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult Factura()
 		{
-			return View();
-			int a = 2;
-			if (a > 1)
-			{
+            var factura = new Factura
+            {
+                NumeroFactura = "001",
+                NombreCliente = "Juan Pérez",
+                TelefonoCliente = "1234-5678",
+                DireccionCliente = "Calle Ejemplo 123",
+                Productos = new List<Producto>
+            {
+                new Producto { Descripcion = "Producto 1", Cantidad = 2, Precio = 10.50m },
+                new Producto { Descripcion = "Producto 2", Cantidad = 1, Precio = 20.00m }
+            },
+                SubTotal = 41.00m,
+                IVA = 8.20m,
+                Total = 49.20m
+            };
 
-			}
-			//Hola
-			//fegfgesg
+            return View(factura);
 
-		}
+        }
 	}
 }
