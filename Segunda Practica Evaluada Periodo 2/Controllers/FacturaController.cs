77b0fc6541b05e12adb08c250112fd8a42526aa6
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-05 23:08:41 -0600

    Revert "Ultimos cambios"
    This reverts commit cc020fd5e6398807a1bd32faef307a9fb228635b.

--- Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
+++ Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
@@ -1,30 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Segunda_Practica_Evaluada_Periodo_2.Models;
 
 namespace Segunda_Practica_Evaluada_Periodo_2.Controllers
 {
 	public class FacturaController : Controller
 	{
-		public IActionResult Factura()
+		public IActionResult Index()
 		{
-            var factura = new Factura
-            {
-                NumeroFactura = "001",
-                NombreCliente = "Juan Pérez",
-                TelefonoCliente = "1234-5678",
-                DireccionCliente = "Calle Ejemplo 123",
-                Productos = new List<Producto>
-            {
-                new Producto { Descripcion = "Producto 1", Cantidad = 2, Precio = 10.50m },
-                new Producto { Descripcion = "Producto 2", Cantidad = 1, Precio = 20.00m }
-            },
-                SubTotal = 41.00m,
-                IVA = 8.20m,
-                Total = 49.20m
-            };
+			return View();
+			int a = 2;
+			if (a > 1)
+			{
 
-            return View(factura);
+			}
+			//Hola
+			//fegfgesg
 
-        }
+		}
 	}
 }
--- Segunda Practica Evaluada Periodo 2/Models/Factura.cs
+++ Segunda Practica Evaluada Periodo 2/Models/Factura.cs
@@ -2,13 +2,5 @@
 {
 	public class Factura
 	{
-        public string NumeroFactura { get; set; }
-        public string NombreCliente { get; set; }
-        public string TelefonoCliente { get; set; }
-        public string DireccionCliente { get; set; }
-        public List<Producto> Productos { get; set; }
-        public decimal SubTotal { get; set; }
-        public decimal IVA { get; set; }
-        public decimal Total { get; set; }
-    }
+	}
 }
