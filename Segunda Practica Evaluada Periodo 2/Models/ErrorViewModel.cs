87f7170a415a7f9595189d4d3d445502232463f8
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-05 21:53:34 -0600

    Add project files.

---
+++ Segunda Practica Evaluada Periodo 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Segunda_Practica_Evaluada_Periodo_2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
