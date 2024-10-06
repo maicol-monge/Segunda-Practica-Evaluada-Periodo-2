cc020fd5e6398807a1bd32faef307a9fb228635b
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-05 23:00:28 -0600

    Ultimos cambios

--- Segunda Practica Evaluada Periodo 2/Models/Factura.cs
+++ Segunda Practica Evaluada Periodo 2/Models/Factura.cs
@@ -2,5 +2,13 @@
 {
 	public class Factura
 	{
-	}
+        public string NumeroFactura { get; set; }
+        public string NombreCliente { get; set; }
+        public string TelefonoCliente { get; set; }
+        public string DireccionCliente { get; set; }
+        public List<Producto> Productos { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal IVA { get; set; }
+        public decimal Total { get; set; }
+    }
 }
---
+++ Segunda Practica Evaluada Periodo 2/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace Segunda_Practica_Evaluada_Periodo_2.Models
+{
+    public class Producto
+    {
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
