2b40268c55991157bbd179de267af40fd1e4047e
Maicol Monge <maicol.monge@catolica.edu.sv>
2024-10-05 21:56:04 -0600

    Nuevo Controlador

---
+++ Segunda Practica Evaluada Periodo 2/Controllers/FacturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Segunda_Practica_Evaluada_Periodo_2.Controllers
+{
+	public class FacturaController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
